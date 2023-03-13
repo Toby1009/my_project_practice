2bad8f9b7077137d0aadc3b09566902ab33df690
Toby1009 <st99005912@gmail.com>
2023-03-13 21:41:42 +0800

    不知道改了啥

--- example01/lib/home/view/home_page.dart
+++ example01/lib/home/view/home_page.dart
@@ -100,7 +100,8 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                         ],
                       )
                     ],
-                  )),
+                  )
+              ),
             ),
           ],
         ),
